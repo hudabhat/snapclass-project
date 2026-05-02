1c13ab79274650d62105c5bdf3c47c3c0dd0cbd8
hudabhat <hudabhat890@gmail.com>
2026-05-02 20:17:30 +0530

    added shortcut to teacher screen

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -37,7 +37,7 @@ def teacher_dashboard():
         header_dashboard()
     with c2:
         st.subheader(f""" Welcome {teacher_data['name']}""")
-        if st.button("Logout",type='secondary',key='loginbackbtn',shortcut="control+L"):
+        if st.button("Logout",type='secondary',key='loginbackbtn',shortcut="control+L",width='stretch'):
             st.session_state['is_logged_in'] = False
             del st.session_state.teacher_data
             st.rerun()
